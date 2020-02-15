7f6c0373925046e6fb69cd62f437ec1a386826e4
IBM-Cloud-DevOps <60835744+IBM-Cloud-DevOps@users.noreply.github.com>
2020-02-15 13:38:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 exports.handler = (event, context, callback) => {
   console.log('Received event:', JSON.stringify(event, null, 2));
 
-  var message = "Hello cool guy from IBM!";
+  var message = "Hello cool guy from Sony!";
 
   callback(null, {
     statusCode: 200,
