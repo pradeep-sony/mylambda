81e0c48a655cc77582b278c615fd7c31f066e94c
IBM-Cloud-DevOps <60835744+IBM-Cloud-DevOps@users.noreply.github.com>
2020-02-16 11:07:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 exports.handler = (event, context, callback) => {
   console.log('Received event:', JSON.stringify(event, null, 2));
 
-  var message = "Sales Forecast App Ver 1.0";
+  var message = "Sales Forecast App Update 1.0";
 
   callback(null, {
     statusCode: 200,
