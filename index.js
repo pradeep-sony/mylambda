c803e5f97f726f1dc1ead2e4b049396a8105d2c5
pradeep-sony <60911097+pradeep-sony@users.noreply.github.com>
2020-03-01 13:17:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 exports.handler = (event, context, callback) => {
   console.log('Received event:', JSON.stringify(event, null, 2));
 
-  var message = "Sales Forecast App Update 2.0";
+  var message = "Dxinfra App Update 2.0";
 
   callback(null, {
     statusCode: 200,
